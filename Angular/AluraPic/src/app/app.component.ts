0eaa74de848de4e196990aea5c28ec5105a66309
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2018-09-27 18:07:47 -0300

    Alterado para usar rotas do anguler

--- Angular/AluraPic/src/app/app.component.ts
+++ Angular/AluraPic/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +6,6 @@ import { PhotoService } from './photos/photo/photo.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
-  photos: any[] =[];
-
-  constructor(private photoService : PhotoService){}
-  
-  ngOnInit(): void{
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => {
-        this.photos = photos
-      });
-  }
-}
+export class AppComponent{}
 
 
