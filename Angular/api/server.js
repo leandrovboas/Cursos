1554882deae5c9fce4bc1cf4123c048bb688bc53
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2018-09-27 18:12:03 -0300

    alteracao na msg do server

--- Angular/api/server.js
+++ Angular/api/server.js
@@ -2,6 +2,7 @@ var http = require('http')
     ,app = require('./config/express');
     
 http.createServer(app).listen(3000, function() {
+    console.log('NodeJs API Startada');
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
 
