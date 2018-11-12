4d80c60796c2624d8c29e0df319a20fce04ba4c9
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2018-11-12 16:45:48 -0200

    Concluindo terceira parte do curso de angular

--- Angular/AluraPic/src/app/app.routing.module.ts
+++ Angular/AluraPic/src/app/app.routing.module.ts
@@ -5,6 +5,8 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { AuthGuard } from './core/auth/auth.guard';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 
 const routes: Routes = [
     {
@@ -18,6 +20,7 @@ const routes: Routes = [
     },              
     { 
         path: 'user/:userName', 
+        pathMatch: 'full',
         component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
@@ -25,11 +28,20 @@ const routes: Routes = [
     },
     { 
         path: 'p/add', 
-        component: PhotoFormComponent 
+        component: PhotoFormComponent,
+        canActivate: [AuthGuard]
     },
     { 
-        path: '**', 
+        path: 'p/:photoId', 
+        component: PhotoDetailsComponent,
+    }, 
+    { 
+        path: 'not-found', 
         component: NotFoundComponent 
+    },     
+    { 
+        path: '**', 
+        redirectTo: 'not-found'
     }  
 ];
 
--- Angular/AluraPic/src/app/photos/photo-form/photo-form.component.ts
+++ Angular/AluraPic/src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
+import { AlertService } from '../../shared/components/alert/alert.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
   selector: 'ap-photo-form',
@@ -12,11 +14,14 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -32,6 +37,17 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']))
+      .subscribe(() => {
+        this.alertService.success('Upload complete', true);
+        this.router.navigate(['/user', this.userService.getUserName()]);
+      });
+        
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 }
